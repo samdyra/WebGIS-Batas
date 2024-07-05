35cc8db1c0a7f7b5b395c42b02df071624cc0b1a
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2024-07-05 15:47:54 +0700

    feat: re-init v3

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import viteLogo from '/vite.svg';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- frontend/src/Component/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Sidebar } from "./Sidebar";
-export { default as BaseMapPicker } from "./BasemapPicker"
-export { default as MapScreen } from "./Map"
