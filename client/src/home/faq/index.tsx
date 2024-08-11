1c52d141c2398613387aabc2ca2c3119d740f683
raihanmakarim <111282057+raihanmakarim@users.noreply.github.com>
2024-08-11 21:43:10 +0700

    feat: change accordion to shacdn ui ,remove material ui

--- client/src/home/faq/index.tsx
+++ client/src/home/faq/index.tsx
@@ -1,7 +1,8 @@
-'use client';
 import React from 'react';
 
-import { Typography, Accordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
+// import { Typography, Accordion, AccordionHeader, AccordionBody } from '@material-tailwind/react';
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './accordion';
 
 const FAQS = [
   {
@@ -27,32 +28,25 @@ const FAQS = [
 ];
 
 export function Faq() {
-  const [open, setOpen] = React.useState(0);
-  const handleOpen = (value: number) => setOpen(open === value ? 0 : value);
-
   return (
     <section className="px-8 py-40">
       <div className="container mx-auto">
         <div className="text-center">
-          <Typography variant="h1" color="blue-gray" className="mb-4">
-            Frequently Asked Questions
-          </Typography>
-          <Typography variant="lead" className="mx-auto mb-24 w-full max-w-2xl !text-gray-500">
+          <div className="text-3xl font-bold mb-5">Frequently Asked Questions</div>
+          <div className="mx-auto mb-24 w-full max-w-2xl !text-gray-500">
             The Back-to-School Campaign is a special promotion designed to make your return to school as smooth as
             possible.
-          </Typography>
+          </div>
         </div>
         <div className="mx-auto lg:max-w-screen-lg lg:px-20">
-          {FAQS.map(({ title, desc }, key) => (
-            <Accordion key={key} open={open === key + 1} onClick={() => handleOpen(key + 1)}>
-              <AccordionHeader className="text-left text-gray-900">{title}</AccordionHeader>
-              <AccordionBody>
-                <Typography color="blue-gray" className="font-normal text-gray-500">
-                  {desc}
-                </Typography>
-              </AccordionBody>
-            </Accordion>
-          ))}
+          <Accordion type="single" collapsible>
+            {FAQS.map(({ title, desc }, key) => (
+              <AccordionItem value={`item-${key}`} key={key}>
+                <AccordionTrigger>{title}</AccordionTrigger>
+                <AccordionContent>{desc}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
       </div>
     </section>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -20,6 +20,20 @@ export default {
         lg: '3rem', // 48px
         xl: '4rem', // 64px
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [
