4b9ab739014e47a8607ca0272f898d97499ca1a8
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2024-11-19 17:18:52 +0700

    fix: navbar

--- client/src/shared/components/Navbar/index.tsx
+++ client/src/shared/components/Navbar/index.tsx
@@ -15,7 +15,7 @@ const Navbar = ({ className }: { className?: string }) => {
         <ul className="flex flex-col p-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
           <li>
             <a
-              href="/"
+              href="https://pemotda.jabarprov.go.id/id"
               className="block py-2 px-3 text-main-green rounded  md:hover:bg-transparent md:hover:text-main-green-dark md:p-0 font-semibold"
               aria-current="page"
             >
@@ -82,7 +82,7 @@ const Navbar = ({ className }: { className?: string }) => {
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
                 <li>
                   <a
-                    href="/"
+                    href="https://pemotda.jabarprov.go.id/id"
                     className="block py-2 px-3 text-main-green rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-main-green-dark md:p-0 font-semibold"
                     aria-current="page"
                   >
